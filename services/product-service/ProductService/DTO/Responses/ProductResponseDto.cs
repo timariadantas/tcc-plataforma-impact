3097de15f62184417440c1d7bdbec6dc2ead2d38
Maria Dantas <mariadantasti@gmail.com>
2025-12-08 19:19:19 -0300

    add Product Dto

--- services/product-service/ProductService/DTO/Responses/ProductResponseDto.cs
+++ services/product-service/ProductService/DTO/Responses/ProductResponseDto.cs
@@ -6,6 +6,7 @@ namespace ProductService.DTO.Responses
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int Stock { get; set; } 
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public bool Active { get; set; }
