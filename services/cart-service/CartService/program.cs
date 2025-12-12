6fff7aa1497bbc3d80da61cee03446a0b1785e04
Maria Dantas <mariadantasti@gmail.com>
2025-12-12 20:12:20 -0300

    CartService - Program

--- services/cart-service/CartService/program.cs
+++ services/cart-service/CartService/program.cs
@@ -1,34 +1,49 @@
 using CartService.Storage;
 using CartService.Service;
 using CartService.Logging;
-using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Carrega variáveis do ambiente (.env)
-DotNetEnv.Env.Load();
 
-// Connection string
 var configuration = builder.Configuration;
-var connectionString = $"Host={configuration["CART_DB_HOST"]};Port={configuration["CART_DB_PORT"]};Username={configuration["CART_DB_USER"]};Password={configuration["CART_DB_PASSWORD"]};Database={configuration["CART_DB_NAME"]}";
 
-// Logger
-var logger = new LoggerService("/app/Logs/cart-logfile.log");
+// variáveis no docker-compose
+var connectionString =
+    $"Host={configuration["CART_DB_HOST"]};" +
+    $"Port={configuration["CART_DB_PORT"]};" +
+    $"Username={configuration["CART_DB_USER"]};" +
+    $"Password={configuration["CART_DB_PASSWORD"]};" +
+    $"Database={configuration["CART_DB_NAME"]}";
 
-// Injeção de dependências
-builder.Services.AddSingleton(logger); // logger
+
+builder.Services.AddSingleton<ILoggerService>(sp =>
+    new LoggerService("/app/Logs/cart.log"));
+
+
+builder.Services.AddSingleton<ILoggerService>(sp =>  //servidorprovider
+    new LoggerService("/app/Logs/cart.log"));
+
+
+
+//DI
 builder.Services.AddSingleton<ISalesStorage>(new SalesStorage(connectionString));
 builder.Services.AddScoped<ISalesService, SalesService>();
 
-// Swagger e Controllers
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+// Porta de Conf Serviço
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
 
 var app = builder.Build();
 
+// Middleware padrão
 app.UseRouting();
 app.UseAuthorization();
+
+// Swagger 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -36,5 +51,8 @@ app.UseSwaggerUI(c =>
     c.RoutePrefix = "swagger/cart";
 });
 
+// Mapear controllers
 app.MapControllers();
+
+// Rodar aplicação
 app.Run();
